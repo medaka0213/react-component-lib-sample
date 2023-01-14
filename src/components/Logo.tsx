42e4b3d225d471278c5e8a0b065095a97e8e235b
medaka0228 <anywherefish@gmail.com>
2023-01-14 19:49:23 +0900

    React のインポートを修正

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, BoxProps } from '@mui/material';
+import Box, { BoxProps } from '@mui/material/Box';
 
 type LogoProps = BoxProps & {
   variant?: 'jp' | 'en' | 'white';
