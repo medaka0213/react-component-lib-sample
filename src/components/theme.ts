de6498a679551d446e00515b1a1fb5610666b760
medaka <36759068+medaka0213@users.noreply.github.com>
2023-05-28 11:51:27 +0900

    secondary 削除

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -4,15 +4,10 @@ const theme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: "#004F8A",
-      light: "#0063ac",
-      dark: "#003f6e",
+      main: '#004F8A',
+      light: '#0063ac',
+      dark: '#003f6e',
     },
-    secondary: {
-      main: "#dadde8",
-      light: "#bac3d3",
-      dark: "#63779b",
-    }
   },
 });
 
