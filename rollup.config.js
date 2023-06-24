3e93f6523db508cb5b80e35dfa2d6fd8098cb8e9
medaka <36759068+medaka0213@users.noreply.github.com>
2023-06-24 19:39:58 +0900

    ビルド手順変更

--- rollup.config.js
+++ rollup.config.js
@@ -1,82 +1,106 @@
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import path from 'path';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import glslify from 'rollup-plugin-glslify';
+import multiInput from 'rollup-plugin-multi-input';
+import { terser } from 'rollup-plugin-terser';
+
+const root = process.platform === 'win32' ? path.resolve('/') : '/';
+const external = (id) => !id.startsWith('.') && !id.startsWith(root);
+const extensions = ['.js', '.jsx', '.ts', '.tsx', '.json'];
+
+const getBabelOptions = ({ useESModules }) => ({
+  babelrc: false,
+  extensions,
+  exclude: '**/node_modules/**',
+  babelHelpers: 'runtime',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        include: [
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-optional-chaining',
+          '@babel/plugin-proposal-nullish-coalescing-operator',
+          '@babel/plugin-proposal-numeric-separator',
+          '@babel/plugin-proposal-logical-assignment-operators',
+        ],
+        bugfixes: true,
+        loose: true,
+        modules: false,
+        targets: '> 1%, not dead, not ie 11, not op_mini all',
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    ['@babel/transform-runtime', { regenerator: false, useESModules }],
+  ],
+});
 
 export default [
   {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/cjs/index.js',
-        format: 'cjs',
-        sourcemap: false,
-      },
-      {
-        file: 'dist/esm/index.js',
-        format: 'esm',
-        sourcemap: false,
-      },
-    ],
+    input: ['src/**/*.ts', 'src/**/*.tsx', '!src/index.ts'],
+    output: { dir: `dist`, format: 'esm' },
+    external,
     plugins: [
-      commonjs({
-        include: ['node_modules/**'],
-      }),
-      typescript({
-        tsconfig: './tsconfig.json',
-        exclude: ['**/__tests__/**'],
-      }),
-      nodeResolve(),
+      multiInput(),
       json(),
+      glslify(),
+      babel(
+        getBabelOptions(
+          { useESModules: true },
+          '>1%, not dead, not ie 11, not op_mini all'
+        )
+      ),
+      resolve({ extensions }),
     ],
-    external: ['react', 'react-dom', '@emotion/styled', '@emotion/react'],
   },
   {
-    input: 'src/models/index.ts',
-    output: [
-      {
-        file: 'dist/cjs/models.js',
-        format: 'cjs',
-        sourcemap: false,
-      },
-      {
-        file: 'dist/esm/models.js',
-        format: 'esm',
-        sourcemap: false,
-      },
-    ],
+    input: `./src/index.ts`,
+    output: { dir: `dist`, format: 'esm' },
+    external,
     plugins: [
-      commonjs({
-        include: ['node_modules/**'],
-      }),
-      typescript({
-        tsconfig: './tsconfig.json',
-        exclude: ['**/__tests__/**'],
-      }),
-      nodeResolve(),
       json(),
+      glslify(),
+      babel(
+        getBabelOptions(
+          { useESModules: true },
+          '>1%, not dead, not ie 11, not op_mini all'
+        )
+      ),
+      resolve({ extensions }),
     ],
-    external: ['react', 'react-dom', '@emotion/styled', '@emotion/react'],
+    preserveModules: true,
   },
   {
-    input: 'dist/cjs/index.d.ts',
-    output: [{ file: 'dist/cjs/index.d.ts', format: 'cjs' }],
-    plugins: [dts()],
+    input: ['src/**/*.ts', 'src/**/*.tsx', '!src/index.ts'],
+    output: { dir: `dist`, format: 'cjs' },
+    external,
+    plugins: [
+      multiInput({
+        transformOutputPath: (output) => output.replace(/\.[^/.]+$/, '.cjs.js'),
+      }),
+      json(),
+      glslify(),
+      babel(getBabelOptions({ useESModules: false })),
+      resolve({ extensions }),
+      terser(),
+    ],
   },
   {
-    input: 'dist/esm/index.d.ts',
-    output: [{ file: 'dist/esm/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
-  },
-  {
-    input: 'dist/cjs/models/index.d.ts',
-    output: [{ file: 'dist/cjs/models.d.ts', format: 'cjs' }],
-    plugins: [dts()],
-  },
-  {
-    input: 'dist/esm/models/index.d.ts',
-    output: [{ file: 'dist/esm/models.d.ts', format: 'esm' }],
-    plugins: [dts()],
+    input: `./src/index.ts`,
+    output: { file: `dist/index.cjs.js`, format: 'cjs' },
+    external,
+    plugins: [
+      json(),
+      glslify(),
+      babel(getBabelOptions({ useESModules: false })),
+      resolve({ extensions }),
+      terser(),
+    ],
   },
 ];
