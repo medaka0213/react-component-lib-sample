a2800adba4cd960b7076551f9e39bd39b956e187
medaka <36759068+medaka0213@users.noreply.github.com>
2023-09-05 22:59:24 +0900

    youtube_url 追加

--- src/models/youtubeTranscript.ts
+++ src/models/youtubeTranscript.ts
@@ -2,6 +2,7 @@ import { BaseModel, Fields } from './baseModel';
 
 export class YoutubeTranscript extends BaseModel {
   public readonly youtube_id: string = '';
+  public readonly youtube_url: string = '';
   public readonly youtube_title: string = '';
   public readonly content_list: any[] = [];
   public readonly created_at: string = '';
@@ -10,5 +11,11 @@ export class YoutubeTranscript extends BaseModel {
   constructor(props: Fields<YoutubeTranscript>) {
     super(props);
     Object.assign(this, props);
+  }
+
+  url() {
+    return (
+      this.youtube_url || `https://www.youtube.com/watch?v=${this.youtube_id}`
+    );
   }
 }
