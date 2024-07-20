af35a9f1b3f2f161b8412df33c3cd942bbd2c51f
medaka <36759068+medaka0213@users.noreply.github.com>
2024-07-21 01:01:40 +0900

    ビルド失敗対策

--- src/components/CountDownClock.tsx
+++ src/components/CountDownClock.tsx
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 import { format_countdown } from '../utils/time';
 
 export type CountDownClockProps = {
   datetime_iso: string;
-  children: any;
 };
 
-export const CountDownClock: React.VFC<CountDownClockProps> = ({
-  datetime_iso,
-  children,
-}: CountDownClockProps) => {
+export const CountDownClock: FC<CountDownClockProps> = ({ datetime_iso }) => {
   const [time_format, setTimeFormat] = useState(format_countdown(datetime_iso));
 
   // 0.1秒ごとにvalueを1ずつ増やす
@@ -20,7 +16,7 @@ export const CountDownClock: React.VFC<CountDownClockProps> = ({
     return () => clearInterval(timer);
   }, []);
 
-  return <span>{time_format || children}</span>;
+  return <span>{time_format}</span>;
 };
 
 export default CountDownClock;
